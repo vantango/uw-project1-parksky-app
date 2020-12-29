98824c25d252c5e0e32cc2742a0b98b247361659
Angel <skelliebunnie@gmail.com>
2020-12-29 13:06:22 -0800

    LocalStorage Function Added - added function to save parkCode, lat/lon, and date to localStorage - updated displayData function and script.js to use saveLocalStorage function - alphabetized list of parks - added remaining parks (11 were missing) - added page links for park details and star chart pages - copied index.html to parkinfo.html and starchart.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
 $("document").ready(function() {
 	$("#visitDate").val(dayjs().format("YYYY-MM-DD"));
 
-	// get the currently selected parkCode
-	var code = $("#searchParksSelect").val();
-	var date = dayjs($("#visitDate").val());
+	var prevData = JSON.parse(localStorage.getItem('parksky-data')) || null;
+	var parkCode, date;
+
+	if(prevData === null) {
+		parkCode = "acad";
+		date = dayjs().format("YYYY-MM-DD");
+		saveLocalStorage(null);
+	} else {
+		parkCode = prevData[0];
+		date = prevData[3];
+	}
+
+	$("#searchParksSelect").val(parkCode);
+	$("#visitDate").val(date);
+
 	// display the star chart and parkInfo, but NOT the
 	// starChart details or park Details
-	displayData(code, true, false, true, false);
+	displayData(parkCode, true, false, true, false);
 
 	// on park change, update both parkInfo and star chart
 	$("#searchParksSelect").change(function() {
@@ -15,6 +27,17 @@ $("document").ready(function() {
 
 	// on date change, only update the star chart
 	$("#visitDate").change(function() {
-		displayData($("#searchParksSelect").val(), true, false, false, false);
+		date = $(this).val();
+		// relace the prevData date with the new date
+		prevData.splice(3,1,$(this).val());
+		// save to localStorage
+		saveLocalStorage(prevData);
+		// get the star chart!
+		getStarChart({
+			fullName: "n/a",
+			latitude: prevData[1],
+			longitude: prevData[2],
+			date: dayjs(date)
+		});
 	});
 });
