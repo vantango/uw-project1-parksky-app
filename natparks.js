81c7c63b0b478c67f5d78341323878839bf52260
Aleks Kimball-Moreno <alekskimball@gmail.com>
2020-12-29 16:02:50 -0800

    added description paragraph from wiki api to park details page

--- natparks.js
+++ natparks.js
@@ -1,10 +1,10 @@
-var apiKey = "xPkKTrKpbGvpcurV2SJBVh1pC5nv5z4CboFrtrFO"
+// var apiKey = "xPkKTrKpbGvpcurV2SJBVh1pC5nv5z4CboFrtrFO"
 
-var natPark = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=" + apiKey;
+// var natPark = "https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=" + apiKey;
 
-$.ajax({
-    url: natPark,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-})
+// $.ajax({
+//     url: natPark,
+//     method: "GET"
+// }).then(function (response) {
+//     console.log(response);
+// })
