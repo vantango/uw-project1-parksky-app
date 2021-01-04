a9df2f853e50f4e2a1011b433746459baec3f3a8
Jessica Dao <xjessicadao@gmail.com>
2021-01-03 17:16:28 -0800

    moved original files to seperate folder, created new files for materialize

---
+++ assets/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+})(jQuery); // end of jQuery name space
